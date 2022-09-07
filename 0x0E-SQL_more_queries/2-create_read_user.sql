4ace9e54d9839c2aea75093ec87e367d50dc0cb4
kristineatease1 <kristineatease@gmail.com>
2022-09-07 15:18:25 -0700

    error fix 1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
--- creates the database hbtn_0d_2 and the user user_0d_2C
+-- MrSQL script to create Database and User
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.*
+TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
