1def70bf98e39c93b048b087af5831af43a97787
kristineatease1 <kristineatease@gmail.com>
2022-09-07 15:12:48 -0700

    error fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Create a new user for MySQL and grant all privileges
-GRANT ALL PRIVILEGES ON * . *
-TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
