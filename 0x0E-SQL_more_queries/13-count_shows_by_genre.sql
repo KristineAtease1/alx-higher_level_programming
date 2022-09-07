661b212477827b6ecc8efc37a4c7e5b2f4f515ff
kristineatease1 <kristineatease@gmail.com>
2022-09-07 15:22:24 -0700

    error fix 2

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
--- MySQL script to select shows and value
-SELECT name AS 'genre', COUNT(*) AS 'number_shows'
-FROM tv_genres, tv_show_genres
-WHERE tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT a.name AS genre, COUNT(b.genre_id) AS number_of_shows
+FROM tv_genres a
+JOIN tv_show_genres b
+ON a.id = b.genre_id
+GROUP BY b.genre_id
+ORDER BY number_of_shows DESC;
