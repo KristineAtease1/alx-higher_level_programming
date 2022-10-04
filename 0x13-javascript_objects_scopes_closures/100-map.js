281c5a1210058dd7fcaefee8b8aa076cdbc5bd5f
kristineatease1 <kristineatease@gmail.com>
2022-10-04 15:41:30 -0700

    trial again

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-const array = require('./100-data').list;
-
-console.log(array);
-let cont = 0;
-const map1 = array.map(function (x) {
-  return (x * cont++);
-});
-console.log(map1);
+const origin = require('./100-data').list;
+const newl = origin.map((e, idx) => e * idx);
+console.log(origin);
+console.log(newl);
