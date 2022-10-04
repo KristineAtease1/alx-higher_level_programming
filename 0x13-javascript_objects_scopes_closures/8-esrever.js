732a44e33202af21569c2cc265af0f5656d000ac
kristineatease1 <kristineatease@gmail.com>
2022-10-04 15:26:17 -0700

    nine

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+// function that returns the reversed version of a list
+
 exports.esrever = function (list) {
-  for (let i = 0; i < list.length / 2; i++) {
-    let temp = list[i];
-    list[i] = list[list.length - i - 1];
-    list[list.length - i - 1] = temp;
+
+  let rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
   }
-  return list;
+  return rlist;
 };
