e7c10f67fe83600c5a26dbdfa9fd3bbb6a7adc2b
kristineatease1 <kristineatease@gmail.com>
2022-10-20 16:40:00 -0700

    correct

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl "$1" -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# Script that takes URL and sends aPOst request
+curl -sL -X POST -d 'email=test%40gmail.com&subject=I+will+always+be+here+for+PLD' "$1"
