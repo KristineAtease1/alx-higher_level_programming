8b3f3005dbbd771d1039e01fb3cff16b2ff573de
kristineatease1 <kristineatease@gmail.com>
2022-10-23 11:37:53 -0700

    two

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response """
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as r:
+        html = r.read()
+        print('{}'.format(html.decode('utf-8')))
