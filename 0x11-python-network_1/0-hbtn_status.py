1d3c73fb3774fe125ec10ebc5b24d240fb05021c
kristineatease1 <kristineatease@gmail.com>
2022-10-23 13:09:11 -0700

    fixed again

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script that fetches from a url """
-import urllib.request as req
+""" Script that fetches from a url using urllib package"""
 
+import urllib.request as request
 
-fetched = req.Request('https://alx-intranet.hbtn.io/status')
-with req.urlopen(fetched) as ff:
-    content = ff.read()
-    print("Body response:")
-    print("    -type: {}".format(type(content)))
-    print("    -content: {}".format(content))
-    print("    -utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
